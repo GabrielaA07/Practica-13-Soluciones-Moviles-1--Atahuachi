e321259b809316d1df572e3584ec2839c363e0e7
Gabriela <gabatahuachir@upt.pe>
2020-12-10 15:26:54 -0500

    Práctica 13 - Versión 04

---
+++ app/src/main/java/com/example/pushnotifications/FirebaseService.java
@@ -0,0 +1,22 @@
+package com.example.pushnotifications;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseService extends FirebaseInstanceIdService {
+    String TAG = "Firebase PushNotification: ";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
